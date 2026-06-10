8c4b2bdbaa0caeaf9b56af83946d2ea7d1def7d4
Broken Pipe <navigaid@gmail.com>
2026-06-10 20:46:25 +0800

    allow all headers
    fix: header ‘user-agent’ is not allowed according to header ‘Access-Control-Allow-Headers’ from CORS preflight response

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ func cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Accept-Encoding")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
